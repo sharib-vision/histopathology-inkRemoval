986d52c781cdaf888bcbc2aae8cfa91cb228c26d
sharibox <sharib.ali@eng.ox.ac.uk>
2018-10-07 19:09:15 +0100

    test script updated

--- scripts/runscriptOnTestImages.sh
+++ scripts/runscriptOnTestImages.sh
@@ -1,16 +1,20 @@
 
 #!\bin\bash
 
+# Activate your environment with pytorch
 source activate $ENVIRONMENT
 
-BASEDIR=HISTOPATHOLOGY-PIPELINE-CHECKPOINTS
+BASEDIR="$(pwd)"
+#HISTOPATHOLOGY-PIPELINE-CHECKPOINTS
 RESULTDIR=$BASEDIR/results
 
 `mkdir -p $RESUTLDIR`
 
-python test_cycleGAN.py --dataroot ./$BASEDIR/test_ISBI/ \
+cd $BASEDIR/codes
+
+python test_cycleGAN.py --dataroot $BASEDIR/test_ISBI/ \
 --model test --loadSize 512 --fineSize 512  --how_many 3 \
 --results_dir $RESULTDIR \
---checkpoints_dir .$BASEDIR/checkpoint_SPARSE/
+--checkpoints_dir $BASEDIR/checkpoint_SPARSE/
 
 
